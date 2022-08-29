469bce4a3272853bcc568f8b8ddb4249d463bdc0
Colin Cameron <ccameron2020@gmail.com>
2022-08-29 16:42:04 +0100

    First Commit

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,12 +1,12 @@
-name = "Colin "
+name = "colin "
 age = 33
-info = "My name is " + name + "and my age is " + str(age) + "."
+info = "My name is " + name.capitalize() + "and my age is " + str(age) + "."
 print (info)
 
 dogyears = age * 7
 print("My age in dog years is roughly "+str(dogyears))
 
-statment = 1
+statment = 0
 if statment == True:
     print("This statment is true")
 else:
